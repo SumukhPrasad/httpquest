7346390580a0df4400ea06272b59d739ffe93a02
Sumukh Prasad <sumukhprasad@icloud.com>
2024-02-10 20:46:37 +0530

    Modified game view files.

---
+++ HTTPQuest.swiftpm/Game/Game/Game.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Sumukh Prasad on 10/02/24.
+//
+
+import SwiftUI
+
+struct Game: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Game()
+}
--- HTTPQuest.swiftpm/Game/GameView.swift
+++ HTTPQuest.swiftpm/Game/GameView.swift
@@ -9,14 +9,60 @@ import SwiftUI
 
 struct GameView: View {
     @Binding var tabSelection: Int
-    @State private var isSheetPresented: Bool = true
+    @State private var isInstructionSheetPresented: Bool = true
+    @State private var isExitSheetPresented: Bool = false
     
     var body: some View {
-        Text("Hello, World! gv")
-        .sheet(isPresented: $isSheetPresented) {
-            // Content of your sheet goes here
-            GameInstructionsView()
+        
+        VStack {
+            
+            Game()
+            
+            Spacer()
+            
+            HStack {
+                Button(action: {isInstructionSheetPresented=true}, label: {
+                    HStack {
+                        Text("Instructions")
+                        Image(systemName: "doc.questionmark.fill")
+                    }
+                }).buttonStyle(.bordered)
+                .sheet(isPresented: $isInstructionSheetPresented) {
+                    VStack {
+                        ScrollView {
+                                GameInstructionsView()
+                        }
+                        
+                        Button(action: {isInstructionSheetPresented=false}, label: {
+                            HStack {
+                                Text("Play")
+                                Image(systemName: "play.fill")
+                            }
+                        }).buttonStyle(.borderedProminent)
+                    }.padding(20)
+                    
+                }
+                
+                Spacer()
+                
+                Button(action: {isExitSheetPresented=true}, label: {
+                    HStack {
+                        Text("Exit")
+                        Image(systemName: "rectangle.portrait.and.arrow.forward")
+                    }
+                }).buttonStyle(.bordered)
+                .alert("Exit Game", isPresented: $isExitSheetPresented) {
+                    Button("No, take me back", role: .destructive) { isExitSheetPresented = false }
+                    Button("Yes, exit", role: .cancel) {
+                        isExitSheetPresented = false
+                        tabSelection = 3
+                    }
+                } message: {
+                    Text("Are you sure you want to exit?")
+                }
+            }
         }
+        .padding(20)
     }
         
 }
