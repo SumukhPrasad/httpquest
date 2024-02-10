1261e7313f690b236c5d65ccb2d56694c4c73105
Sumukh Prasad <sumukhprasad@icloud.com>
2024-02-10 20:49:02 +0530

    Added conclusion content view.

---
+++ HTTPQuest.swiftpm/Conclusion/ConclusionContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ConclusionContentView.swift
+//
+//
+//  Created by Sumukh Prasad on 10/02/24.
+//
+
+import SwiftUI
+
+struct ConclusionContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConclusionContentView()
+}
--- HTTPQuest.swiftpm/Conclusion/ConclusionView.swift
+++ HTTPQuest.swiftpm/Conclusion/ConclusionView.swift
@@ -11,7 +11,39 @@ struct ConclusionView: View {
     @Binding var tabSelection: Int
     
     var body: some View {
-        Text("Hello, World! cv")
+        HStack {
+            ZStack(alignment: .leading) {
+                Color.yellow
+                
+                VStack(alignment: .leading) {
+                    Text("RECAP")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                        .opacity(0.5)
+                    
+                    Text("Let's recap!")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .opacity(0.7)
+                    Spacer()
+                }.padding(20)
+            }
+            .frame(width: UIScreen.main.bounds.width * 0.4)
+            
+            
+            
+            ScrollView {
+                VStack {
+                    ConclusionContentView()
+                    
+                    TextAndIconView(text: "Slide to move forward", systemImage: "chevron.right", imagePosition: .right)
+                }
+                .padding(20)
+                .frame(maxWidth: .infinity)
+            }
+            
+        }.ignoresSafeArea()
     }
 }
 
