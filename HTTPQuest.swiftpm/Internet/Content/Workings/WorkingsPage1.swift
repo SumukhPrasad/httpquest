ff657b018a741a1f125421c4d70c4aaaf690cc7d
Sumukh Prasad <sumukhprasad@icloud.com>
2024-02-22 21:49:43 +0530

    Minor changes.

--- HTTPQuest.swiftpm/Internet/Content/Workings/WorkingsPage1.swift
+++ HTTPQuest.swiftpm/Internet/Content/Workings/WorkingsPage1.swift
@@ -16,7 +16,10 @@ struct WorkingsPageOne: View {
             .fontWeight(.bold)
             .padding(.top, 20.0)
         
-        Text("The moment your message was sent, it starts on a hjourney around the world. ")
+        Text("The moment your message was sent, it starts on a journey around the world. First, your phone takes that message and cuts it up into smaller pieces called \"packets.\"")
+            .multilineTextAlignment(.center)
+        
+        // -----
     }
 }
 
